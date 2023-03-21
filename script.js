3501cdd8a3656e2163da2d74c29ef9ccb36261cb
Ben <benjamin.h.thomas1@gmail.com>
2023-03-21 17:05:26 +1100

    Added Missing Buttons and Calculator functionality
    Added 0,(,) and .
    Added in math.evaluate to securely evaluate the stored string as an expression

--- script.js
+++ script.js
@@ -1,15 +1,57 @@
 const display = document.querySelector(".display")
 const buttons = document.querySelectorAll(".button")
-//const numArray = Array.from(Array(10).keys()) //=> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 buttons.forEach(button => button.addEventListener('click', function onClick(){
-    console.log(button.id);
+    //console.log(button.id);
     let but = button.id;
+
+    if(display.innerHTML.indexOf("=") !== -1){
+        display.innerHTML = '';
+    };
+
+    let currentHTML = display.innerHTML;
+
     if (isNaN(but)) {
         // do something with word buttons
         switch(but){
             case("clear"):
                 display.innerHTML = ''
+                break;
+            case("openBrack"):
+                display.insertAdjacentHTML('beforeend','(');
+                break;
+            case("closeBrack"):
+                display.insertAdjacentHTML('beforeend',')');
+                break;
+            case("dot"):
+                display.insertAdjacentHTML('beforeend','.');
+                break;
+            case("multiply"):
+                display.insertAdjacentHTML('beforeend',"*");
+                break;
+            case("minus"):
+                display.insertAdjacentHTML('beforeend',"-");
+                break;
+            case("plus"):
+                display.insertAdjacentHTML('beforeend',"+");
+                break;
+            case("divide"):
+                display.insertAdjacentHTML('beforeend',"/");
+                break;
+            case("backspace"):
+                if (currentHTML.length > 0){
+                    const newHTML = currentHTML.slice(0,-1); //remove last char
+                    display.innerHTML = newHTML;
+                }
+                break;
+            case("equals"):
+                let value;
+                try {
+                    value = math.evaluate(currentHTML); 
+                } catch (e) {
+                    value = "N/A"
+                };
+                display.insertAdjacentHTML('beforeend',`<br>= ${value}`);
                 break;
         }
     } else {
