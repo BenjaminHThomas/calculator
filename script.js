600537dfc92147689b6ab5bbe09f19815f9aaf62
Ben <benjamin.h.thomas1@gmail.com>
2023-03-23 00:10:08 +1100

    Added functionality to add onto previous answer
    Also added gradient background.

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const display = document.querySelector(".display")
 const buttons = document.querySelectorAll(".button")
+let value = '';
+
+function getExpressionAfterBR(string){
+    //Returns the first non-whitespace character and full expression after a line break
+    const tag = "<br>"
+    // Get new expression
+    const startIndex = string.lastIndexOf(tag) + tag.length;
+    const substring = string.substr(startIndex);
+    return [substring.trim()[0], substring];
+};
+
+function evaluateExpression(string){
+    //executes string expression
+    try {
+        const tempVal = math.evaluate(string); 
+        return tempVal;
+    } catch (e) {
+        return ''; // if there's an error, return nothing.
+    };
+}
 
 buttons.forEach(button => button.addEventListener('click', function onClick(){
-    //console.log(button.id);
-    let but = button.id;
-
-    if(display.innerHTML.indexOf("=") !== -1){
-        display.innerHTML = '';
-    };
-
+    const but = button.id;
     let currentHTML = display.innerHTML;
 
     if (isNaN(but)) {
@@ -39,24 +53,38 @@ buttons.forEach(button => button.addEventListener('click', function onClick(){
                 display.insertAdjacentHTML('beforeend',"/");
                 break;
             case("backspace"):
-                if (currentHTML.length > 0){
+                if (currentHTML.length > 0){ // if there are characters to remove
                     const newHTML = currentHTML.slice(0,-1); //remove last char
-                    display.innerHTML = newHTML;
-                }
+                    display.innerHTML = newHTML; 
+                };
                 break;
             case("equals"):
-                let value;
-                try {
-                    value = math.evaluate(currentHTML); 
-                } catch (e) {
-                    value = "N/A"
-                };
-                display.insertAdjacentHTML('beforeend',`<br>= ${value}`);
+                const [firstChar, cutExpr] = getExpressionAfterBR(currentHTML) // get first character of new line and the new math expression
+                if (currentHTML.indexOf("<br>=") === -1){ // if there's no previous expression
+                    value = evaluateExpression(currentHTML); // execute formula
+                } else if(cutExpr === ''){
+                    // equals pressed with no value entered
+                    display.insertAdjacentHTML('beforeend',`<br>= ${value}<br>`) // enter previously calculated value
+                    break;
+                } else if(value === ''){
+                    value = evaluateExpression(cutExpr); // if previous expression is N/A then only execute new line
+                } else {
+                    if (!isNaN(firstChar)){
+                        // if the first character is a number, ignore the stored value from previous expression
+                        value = evaluateExpression(cutExpr);
+                    } else {
+                        // first char is a mathematical operator, attempt to modify it using operators in new line
+                        value = value.toString();
+                        value = evaluateExpression(value.concat(cutExpr));
+                    };
+                }
+                value==='' ? display.insertAdjacentHTML('beforeend',"<br>= N/A<br>") : display.insertAdjacentHTML('beforeend',`<br>= ${value}<br>`); 
                 break;
         }
     } else {
-        display.insertAdjacentHTML('beforeend', parseFloat(but))
+        display.insertAdjacentHTML('beforeend', but) // add on number to string
     }
-}));
+    //Auto scroll to the bottom when button pressed
+    display.scrollTop = display.scrollHeight - display.clientHeight
 
-// Add text to the display
+}));
